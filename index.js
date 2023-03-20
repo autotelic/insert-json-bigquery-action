56fe9427a941e8eb979e876f453f067f13a8eaed
jeff-sexton <sexton.jeffp@gmail.com>
2023-03-20 15:17:50 -0700

    feat: inital row values

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
+// const { BigQuery } = require('@google-cloud/bigquery');
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // Get BigQuery Inputs
+    // const application_credentials = core.getInput('application_credentials')
+    // const projectId = core.getInput('projectId')
+    // const datasetId = core.getInput('datasetId')
+    // const tableId = core.getInput('tableId')
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // const bigQueryOptions = {
+    //   keyFilename: '/tmp/account.json',
+    //   projectId: projectId,
+    // };
 
-    core.setOutput('time', new Date().toTimeString());
+    // // Create an authorized BigQuery client
+    // const bigqueryClient = new BigQuery(bigQueryOptions)
+
+    // Get Report Data Inputs
+    const reportFilename = core.getInput('reportFilename')
+    const projectName = github.context.repository
+    const branch = github.context.ref_name
+    const sha = github.context.sha
+    const reportTime = github.context.event.created_at
+
+    const rows = [
+      {
+        projectName,
+        branch,
+        sha,
+        reportTime,
+        report: JSON.stringify({reportFilename})
+      }
+    ]
+    // Insert data into a table
+    // core.info(`Inserting report ${reportFilename} into BigQuery Table ${tableId}`);
+
+    core.info(rows)
+    // await bigqueryClient
+    //   .dataset(datasetId)
+    //   .table(tableId)
+    //   .insert(rows)
+
   } catch (error) {
     core.setFailed(error.message);
   }
